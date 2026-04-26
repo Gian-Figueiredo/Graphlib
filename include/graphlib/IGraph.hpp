e03a6b3b3db18e1bc6a037c5bedb1695505c6015
Gian-Fedora <gian78367@gmail.com>
2026-04-26 02:13:36 -0300

    feat: Adição de uma Interface de Graph
    Uma grafo pode ter duas implementações básicas, uma feita por matrix de adjacência e outra por lista de adjacência. Esse arquivo contém uma classe abstrata pura, que possui penas declarações das funções.
    
    Essa interface serve como base para as múltiplas implementações

---
+++ include/graphlib/IGraph.hpp
@@ -0,0 +1,22 @@
+#ifndef IGRAPH_HPP
+#define IGRAPH_HPP
+
+#include <string>
+#include <vector>
+
+namespace graphlib {
+    class IGraph {
+        public:
+            virtual int V() const = 0;
+            virtual int E() const = 0;
+
+            virtual void addEdge(int v, int w) = 0;
+
+            virtual std::vector<int> adj(int v) const = 0;
+            virtual std::string toString() const = 0;
+            
+            virtual ~IGraph() = default;
+    };
+}
+
+#endif
