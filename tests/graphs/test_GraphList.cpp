26d7c474ff1af234401f17cbc0cf890bb2b7c357
Gian-Fedora <gian78367@gmail.com>
2026-04-26 22:13:21 -0300

    test: Testes da classe GraphList
    testa os métodos da classe GraphList

---
+++ tests/graphs/test_GraphList.cpp
@@ -0,0 +1,14 @@
+#include <graphlib/GraphList.hpp>
+#include <test_utils.hpp>
+#include <iostream>
+
+int main() {
+    graphlib::GraphList g(5);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    graphlib::test::run_contract_tests(g, 5, 2);
+    g.clearEdges();
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    std::cout << g.toString() << "\n";
+}
