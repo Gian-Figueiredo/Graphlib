20830a37e882b531189262edd4c7ac1a84a577fc
Gian-Fedora <gian78367@gmail.com>
2026-05-03 18:08:18 -0300

    feat: Criação das interfaces de grafos e dígrafos
    Além da criação, também ocorreu a realozação nas pastas graph e digraph respectivamente

---
+++ include/graphlib/digraph/IDigraph.hpp
@@ -0,0 +1,11 @@
+#ifdef IDIGRAPH_HPP
+#define IDIGRAPH_HPP
+
+#include <graphlib/common/IBaseGraph.hpp>
+
+namespace graphlib {
+    class IDigraph : public IBaseGraph {
+    };
+}
+
+#endif
--- include/graphlib/graph/IGraph.hpp
+++ include/graphlib/graph/IGraph.hpp
@@ -1,22 +1,10 @@
 #ifndef IGRAPH_HPP
 #define IGRAPH_HPP
 
-#include <string>
-#include <vector>
+#include <graphlib/common/IBaseGraph.hpp>
 
 namespace graphlib {
-    class IGraph {
-        public:
-            virtual int V() const = 0;
-            virtual int E() const = 0;
-
-            virtual void addEdge(int v, int w) = 0;
-            virtual void clearEdges() = 0;
-
-            virtual std::vector<int> adj(int v) const = 0;
-            virtual std::string toString() const = 0;
-
-            virtual ~IGraph() = default;
+    class IGraph : public IBaseGraph {
     };
 }
 
