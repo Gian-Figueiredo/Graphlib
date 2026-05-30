c1798cc246bb7210c86f15bcfaf0e4973cf68ec7
Gian-Fedora <gian78367@gmail.com>
2026-05-29 23:20:39 -0300

    feat: criação dos métodos degree e hasEdge
    O método degree calcula o grau dos vértices enquanto o hasEdge retorna se existe uma aresta que liga os vértice v e w

--- include/graphlib/graph/IGraph.hpp
+++ include/graphlib/graph/IGraph.hpp
@@ -5,6 +5,21 @@
 
 namespace graphlib {
     class IGraph : public IBaseGraph {
+        public:
+            virtual void addEdge(int v, int w) = 0;
+
+            virtual int degree(int v) const = 0;
+
+            virtual bool hasEdge(int v, int w) const = 0;
+
+            virtual int E() const override {
+                int numVertices = V();
+                int sumDegree = 0;
+                for (int c = 0; c < numVertices; c++) {
+                    sumDegree += degree(c);
+                }
+                return sumDegree / 2;
+            }
     };
 }
 
